2d761a141750e483c7a4326a8c11a897689820ff
yuliia-mykhailova <yuliia.mykhailova.work@gmail.com>
2021-07-13 16:16:27 +0300

    Edited config, added migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object("app.config.Config")
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
-@app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+from . import models
--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:postgres_password@db:5432/hello_flask_dev")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
