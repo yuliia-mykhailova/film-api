b6d3eef39fffe610a752a55cfd91fce3c8e9c311
yuliia-mykhailova <yuliia.mykhailova.work@gmail.com>
2021-07-14 14:33:32 +0300

    Added schemas

---
+++ app/schemas.py
@@ -0,0 +1,33 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+from app.models import User, Director, Genre, Film
+
+
+class UserSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        exclude = ["user_id"]
+        load_instance = True
+        load_only = ("password",)
+
+
+class DirectorSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Director
+        load_instance = True
+        include_fk = True
+
+
+class GenreSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Genre
+        load_instance = True
+
+
+class FilmSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Film
+        exclude = ["film_id"]
+        load_instance = True
+        include_fk = True
+
