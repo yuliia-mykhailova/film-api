3b1963950e9a3f0360dedaebf39719614c9552c2
Yuliia <yuliia.mykhailova.work@gmail.com>
2021-07-15 21:52:33 +0300

    Directors crud

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
 from app import api
 from app.resources.users import UserResource, UserListResource
+from app.resources.directors import DirectorResource, DirectorListResource
 
 
 route = api.add_resource
 
 route(UserListResource, '/users')
 route(UserResource, '/users/<int:user_id>')
+
+# route(DirectorListResource, '/directors')
+# route(DirectorResource, '/director/<int:director_id>')
