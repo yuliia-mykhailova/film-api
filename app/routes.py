63204177b09f90a7889451ea53eb0b76a2d9cf95
Yuliia <yuliia.mykhailova.work@gmail.com>
2021-07-15 22:07:06 +0300

    Uncomment routes for director

--- app/routes.py
+++ app/routes.py
@@ -8,5 +8,5 @@ route = api.add_resource
 route(UserListResource, '/users')
 route(UserResource, '/users/<int:user_id>')
 
-# route(DirectorListResource, '/directors')
-# route(DirectorResource, '/director/<int:director_id>')
+route(DirectorListResource, '/directors')
+route(DirectorResource, '/directors/<int:director_id>')
